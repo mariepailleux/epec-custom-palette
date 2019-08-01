9ab746157d5d199ee16fc63d32034a9faa8e6348
mariepailleux@hotmail.fr <mariepailleux@hotmail.fr>
2019-08-01 14:36:05 +0200

    Marie EPEC

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # epec-custom-palette
+This application is a wordpress plugins.
+Its a custom palette.
+Install the custome palette on extensions and active this application.
+Change the color on custom palette and personalise.
--- epec-custom-palette.php
+++ epec-custom-palette.php
@@ -1,1 +1,56 @@
 <?php
+/*
+Plugin Name: custom palette
+Description: Palette de couleur
+Version: 0.1
+Author: Marie Pailleux
+*/
+
+function mytheme_setup_theme_supported_features() {
+    add_theme_support( 'editor-color-palette', array(
+        array(
+            'name' => __( 'Brand Color 1', 'epec-custom-palette' ),
+            'slug' => 'strong-magenta',
+            'color' => '#6128A1',
+        ),
+        array(
+            'name' => __( 'Brand Color 2', 'epec-custom-palette' ),
+            'slug' => 'light-grayish-magenta',
+            'color' => '#E94B59',
+        ),
+        array(
+            'name' => __( 'Very Dark Gray', 'epec-custom-palette' ),
+            'slug' => 'very-light-gray',
+            'color' => '#111111',
+        ),
+        array(
+            'name' => __( 'White', 'epec-custom-palette' ),
+            'slug' => 'very-dark-gray',
+            'color' => '#FFFFFF',
+        ),
+    ) );
+}
+ 
+add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
+
+function my_scripts() {
+    wp_register_style( 'prefix-style', plugins_url('style.css', __FILE__) );
+    wp_enqueue_style( 'prefix-style' );
+    }
+    add_action('wp_enqueue_scripts','my_scripts');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
